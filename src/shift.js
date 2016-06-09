a64b95f03a7852193529e22e811cfee4c99252ca
Tom Martin <tom@tpgmartin.com>
2016-06-10 00:09:55 +0100

    Recreate basic inline worker example

--- src/shift.js
+++ src/shift.js
@@ -1,5 +1,37 @@
-var Shift = function () {
-  return;  
+var Shift = function (fn) {
+
+    this.blobURL = _createBlob(fn);
+    this.worker = _createWorker(fn, this.blobURL);
+
 };
 
-module.exports = Shift;
+Shift.prototype.start = function () {
+    
+    this.worker.postMessage(undefined);
+
+}
+
+Shift.prototype.release = function () {
+
+    console.log('release');
+    console.log(this.blobURL);
+
+    this.blobURL = window.URL.revokeObjectURL(this.blobURL);
+
+}
+
+function _createBlob (fn) {
+
+    var blob = new Blob([ "self.onmessage = " + fn.toString() ]);
+
+    return window.URL.createObjectURL(blob);
+
+}
+
+function _createWorker (fn, blobURL) {
+
+    return new Worker(blobURL); 
+
+}
+
+// module.exports = Shift;
